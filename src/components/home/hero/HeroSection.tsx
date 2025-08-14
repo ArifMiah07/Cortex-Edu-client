811564e279e102c7454d45f9e1569f411ce1b440
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-15 03:25:00 +0600

    working on hero section

---
+++ src/components/home/hero/HeroSection.tsx
@@ -0,0 +1,29 @@
+export default function HeroSection(){
+    // todo: func
+    // implement all functionalities here
+    /* ==============================
+        /| add user friendly logical animation
+        |/ double check for bugs
+        /| no ai for writing code or suggestions
+        |/ follow the proper documentations for tools and tech that would have use here
+        /| make proper documentation in comment about the function
+        |/ drink water, eat something, take breaks,
+        /| keep hourly working records in hand notebooks or hand note page
+        |/ just set and open ide and !start coding...look over or light review previously written codes, 
+    ================================*/
+
+    return (
+        // actual section: 
+        /*--------------------------
+            | This is Hero Section
+            | This section will use in home page
+            | This section must have to be modern, clean, polished, soft high contrast
+        --------------------------*/
+        <section>
+            {/* Heading */}
+            <div>
+                <h1>Manage Your Course and Learning Resources in a Single Place</h1>
+            </div>
+        </section>
+    )
+}
