70c990544e7aaba831ff7f9fa0ab1111b04f67ad
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-15 17:07:22 +0600

    added primary navbar

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -2,11 +2,62 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
+  // add functionalities
+  /*--------------------------------
+    | add scrolling functionality
+    | show user profile if login?online
+    | 
+    ----------------------------------*/
+  // user status
+  const user = true;
+  // menu bar content
+  const activeUserNavLinkName = [
+    "Home",
+    "Courses",
+    "Library",
+    "Dashboard",
+    "Profile",
+    "Login",
+  ];
+  const viewerNavLinkName = ["Home", "Courses", "Library", "Register"];
+  // reuseable nav links
+  const navLinks = (
+    <>
+      {user
+        ? activeUserNavLinkName.map((userLinks, indx) => (
+            <li key={indx}>
+              <Link href={`/${userLinks.trim().toLowerCase()}`}>
+                <span>{userLinks}</span>
+              </Link>
+            </li>
+          ))
+        : viewerNavLinkName.map((userLinks, indx) => (
+            <li key={indx}>
+              <Link href={`/${userLinks.trim().toLowerCase()}`}>
+                <span>{userLinks}</span>
+              </Link>
+            </li>
+          ))}
+    </>
+  );
+
+  const a = Array(10).fill(0);
+  console.log(a);
+
   return (
-    <nav className="w-full h-full flex flex-row items-center justify-between gap-3 p-3 border border-red-400   ">
+    // This is Navbar
+    /*---------------------------------
+    | this navbar will used in header section as a shared component
+    | navbar would have 3 main section -- logo, menu, extra link
+    | make navbar responsive for all device
+    | use  a nice logo, make polished ui/ux, 
+    | menu content --> Home, Courses, Library, Dashboard, Profile, Register/Login/Logout, Portfolio
+    | add logical animation and dynamic navbar based on user scroll
+    ----------------------------------*/
+    <nav className="w-full h-full flex flex-row items-center justify-between gap-3 p-6 border border-red-400  ">
       {/* logo */}
       <Link href={"/"}>
-        <div className="flex flex-row items-center justify-center gap-2 p-2">
+        <div className="flex flex-row items-center justify-center gap-2 ">
           <Image
             className={""}
             src={"/assets/images/cortex-dlogo.png"}
@@ -22,9 +73,15 @@ export default function Navbar() {
         </div>
       </Link>
       {/* menu */}
-      <div></div>
+      <div className="flex flex-row gap-4 items-center justify-between ">
+        <ul className="flex flex-row items-center justify-between gap-4 ">
+          {navLinks}
+        </ul>
+      </div>
       {/* extra */}
-      <div></div>
+      <div className="flex flex-row items-center justify-between gap-4">
+        <Link href={"/portfolio"}>Portfolio</Link>
+      </div>
     </nav>
   );
 }
