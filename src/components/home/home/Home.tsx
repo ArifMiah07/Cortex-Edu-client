3017db6c5cb581031b5ae762b78b5cc6a1bc5df9
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-23 20:25:34 +0600

    update home page and footer section

--- src/components/home/home/Home.tsx
+++ src/components/home/home/Home.tsx
@@ -2,8 +2,16 @@
 
 export default function Home(){
     return(
+        // This is Home Page
+        /**
+         * User will redirect here after login
+         * or user can visit this page if features allow the user 
+         * home page will show all the course grid and also the complete course overview for each
+         * 
+         */
     <>
         <h1>this is home page</h1>
+        
     </>
     )
 }
---
+++ src/components/shared/footer/Footer.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Footer(){
+    return(
+        <div>
+            <h1>this is footer section</h1>
+        </div>
+    )
+}
