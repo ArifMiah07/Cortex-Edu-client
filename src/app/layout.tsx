71b5e3aacfee49d9c2c41c0deaeb14771bfd1fd7
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-28 22:46:48 +0600

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 
 import "./globals.css";
+import Navbar from "@/components/shared/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} `}>
+        <header>{/* <Navbar></Navbar> */}</header>
         {children}
         <Toaster richColors />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import LandingPage from "@/components/landing/LandingPage";
 import FooterSection from "@/components/shared/footer/Footer";
-import Navbar from "@/components/shared/navbar/Navbar";
 
 export default function HomePage() {
   return (
     <>
-      <header>
-        <Navbar></Navbar>
-      </header>
       <main>
         <LandingPage />
       </main>
