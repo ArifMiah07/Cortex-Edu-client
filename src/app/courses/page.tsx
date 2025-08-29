4fcc484e56f51561dfbdcfc3e68d0c8ab293437a
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-29 17:57:53 +0600

    added simple course card

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -5,6 +5,7 @@ export default function CoursesPage() {
   type TCourseData = {
     id: number;
     slug: string;
+    image: string;
     title: string;
     description: string;
     mentor: string;
@@ -18,6 +19,7 @@ export default function CoursesPage() {
     {
       id: 1,
       slug: "web-development",
+      image: 'https://i.ytimg.com/vi/nTTmACuMoZ0/hqdefault.jpg',
       title: "Full-Stack Web Development",
       description:
         "Learn to build modern websites and web apps using HTML, CSS, JavaScript, React, and Node.js.",
@@ -30,6 +32,7 @@ export default function CoursesPage() {
     {
       id: 2,
       slug: "app-development",
+      image: 'https://i.ytimg.com/vi/EplH-amHTtE/hqdefault.jpg',
       title: "Mobile App Development",
       description:
         "Master Android and iOS development with React Native and Flutter.",
@@ -42,6 +45,7 @@ export default function CoursesPage() {
     {
       id: 3,
       slug: "software-development",
+      image: 'https://i.ytimg.com/vi/Ubg3PenEdNw/hqdefault.jpg',
       title: "Software Development Fundamentals",
       description:
         "Understand core programming concepts, data structures, and algorithms for software engineering.",
@@ -54,15 +58,15 @@ export default function CoursesPage() {
   ];
 
   return (
-    <>
-      <h1>All Courses</h1>
-      <p>Discover and enroll in courses that match your interests</p>
+    <div className="w-full min-h-screen flex flex-col items-start p-5">
+      <h1 className="text-black text-2xl font-semibold my-4 ">All Courses</h1>
+      <p className="text-gray-500 text-md font-md">Discover and enroll in courses that match your interests</p>
       <div>
         <span>search</span>
       </div>
-      <div>
+      <div className="w-full ">
         <Courses data={data} />
       </div>
-    </>
+    </div>
   );
 }
--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
 //
-type TCourseData = {
+export type TCourseData = {
   id: number;
   slug: string;
+  image: string;
   title: string;
   description: string;
   mentor: string;
@@ -19,14 +21,32 @@ export default function Courses({ data }: { data: TCourseData[] }) {
   return (
     <div>
       <h1>this is courses page</h1>
-      <div className="border border-red-500 p-5">
+      <div className="border border-red-500 p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {data.map((d, i) => (
-          <div className="" key={i}>
-            <h1>{d.slug}</h1>
+          <div className="border border-green-500 p-5 " key={i}>
+            {/* image */}
+            <div className="w-full">
+              <Image
+                src={d.image}
+                width={300}
+                height={300}
+                alt="course image"
+              />
+            </div>
+            {/* heading */}
+            <div className="flex flex-col gap-2">
+              <h1 className="text-black text-lg font-bold mt-4">{d.title}</h1>
+              <p className="text-gray-600 text-sm  " >{d.description}</p>
+              <p className="text-gray-700 font-medium text-sm">by {d.mentor}</p>
+            </div>
+            {/* other info */}
+            <div>
             <p>{d.price}</p>
-            <Link href={`/courses/${d.slug}`}>
-              <button>View Course</button>
-            </Link>
+            </div>
+            {/* action btn */}
+            <button>
+              <Link href={`/courses/${d.slug}`}>View Course</Link>
+            </button>
           </div>
         ))}
       </div>
