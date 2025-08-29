c2b5224f0cc127a47bd671e44e0c1847436e461b
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-29 21:27:38 +0600

    styling button -- css

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -24,9 +24,11 @@ export default function Courses({ data }: { data: TCourseData[] }) {
       <h1>this is courses page</h1>
       <div className="border border-red-500 p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {data.map((d, i) => (
-          <div className=" w-full h-full flex flex-col justify-between border border-green-500 p-5 " key={i}>
+          <div
+            className=" w-full flex flex-col justify-between border border-green-500 p-5 "
+            key={i}>
             {/* image */}
-            <div className="w-full">
+            <div className="w-full flex items-center justify-center">
               <Image
                 src={d.image}
                 width={300}
@@ -34,25 +36,42 @@ export default function Courses({ data }: { data: TCourseData[] }) {
                 alt="course image"
               />
             </div>
+            {/* content */}
             {/* heading */}
-            <div className="flex flex-col">
-              <h1 className="text-black text-lg font-bold mt-4">{d.title}</h1>
+            <div className="flex flex-col ">
+              <h1 className="text-black text-lg font-bold mt-2">{d.title}</h1>
               <p className="text-gray-600 text-sm mt-2 ">{d.description}</p>
-              <p className="text-gray-700 font-medium text-bold">by {d.mentor}</p>
+              <p className="text-gray-700 font-medium text-bold">
+                by {d.mentor}
+              </p>
             </div>
             {/* other info */}
-            <div className="flex flex-row gap-2 justify-between">
-              <span className="flex flex-row gap-[2px] text-sm" ><Star   />{d.rating}</span>
-              <span className="flex flex-row gap-[2px] text-sm" ><Users   />{d.totalEnrollment}</span>
-              <span className="flex flex-row gap-[2px] text-sm" ><Clock4   />{d.courseDuration}</span>
+            <div className="flex flex-row gap-2 justify-between mt-2">
+              <span className="flex flex-row justify-center items-center gap-[2px] text-md">
+                <Star />
+                {d.rating}
+              </span>
+              <span className="flex flex-row justify-center items-center gap-[2px] text-md">
+                <Users />
+                {d.totalEnrollment}
+              </span>
+              <span className="flex flex-row justify-center items-center gap-[2px] text-md">
+                <Clock4 />
+                {d.courseDuration}
+              </span>
             </div>
             {/* action btn */}
-            <div className="flex flex-row gap-3 justify-between">
-              <p>${d.price}</p>
-              <button>
+            <div className="flex flex-row items-center gap-3 justify-between mt-2 ">
+              <p className="text-xl text-yellow-400 font-bold">${d.price}</p>
+              <button className="px-3 py-1 hover:bg-green-500 buy-course-custom-border text-black ">
                 <Link href={`/courses/${d.slug}`}>View Course</Link>
               </button>
+              <button className="px-3 py-1 buy-course-custom-border text-black ">
+                <Link href={`/courses/${d.slug}/payment`}>Buy Course</Link>
+              </button>
             </div>
+            {/* <div className="w-full flex flex-col ">
+            </div> */}
           </div>
         ))}
       </div>
