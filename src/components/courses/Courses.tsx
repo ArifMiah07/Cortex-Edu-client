fc99d0e50397ea726c313b2383bcbbd3519b1267
ArifMiah07 <arifmiah.me322@gmail.com>
2025-08-29 19:20:01 +0600

    styling course vard

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Clock4, Star, Users } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -23,7 +24,7 @@ export default function Courses({ data }: { data: TCourseData[] }) {
       <h1>this is courses page</h1>
       <div className="border border-red-500 p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {data.map((d, i) => (
-          <div className="border border-green-500 p-5 " key={i}>
+          <div className=" w-full h-full flex flex-col justify-between border border-green-500 p-5 " key={i}>
             {/* image */}
             <div className="w-full">
               <Image
@@ -34,19 +35,24 @@ export default function Courses({ data }: { data: TCourseData[] }) {
               />
             </div>
             {/* heading */}
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col">
               <h1 className="text-black text-lg font-bold mt-4">{d.title}</h1>
-              <p className="text-gray-600 text-sm  " >{d.description}</p>
-              <p className="text-gray-700 font-medium text-sm">by {d.mentor}</p>
+              <p className="text-gray-600 text-sm mt-2 ">{d.description}</p>
+              <p className="text-gray-700 font-medium text-bold">by {d.mentor}</p>
             </div>
             {/* other info */}
-            <div>
-            <p>{d.price}</p>
+            <div className="flex flex-row gap-2 justify-between">
+              <span className="flex flex-row gap-[2px] text-sm" ><Star   />{d.rating}</span>
+              <span className="flex flex-row gap-[2px] text-sm" ><Users   />{d.totalEnrollment}</span>
+              <span className="flex flex-row gap-[2px] text-sm" ><Clock4   />{d.courseDuration}</span>
             </div>
             {/* action btn */}
-            <button>
-              <Link href={`/courses/${d.slug}`}>View Course</Link>
-            </button>
+            <div className="flex flex-row gap-3 justify-between">
+              <p>${d.price}</p>
+              <button>
+                <Link href={`/courses/${d.slug}`}>View Course</Link>
+              </button>
+            </div>
           </div>
         ))}
       </div>
