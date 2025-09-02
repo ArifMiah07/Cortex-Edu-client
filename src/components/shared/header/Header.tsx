dfb3a6c38efb5699e9aa08a793f699a528209be2
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-03 01:54:05 +0600

    added sub menu

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Navbar from "../navbar/Navbar";
 import courseCategories from "@/lib/courseCategory";
+import { useState } from "react";
 
 export default function Header() {
   // add all functionalities here
@@ -11,33 +12,53 @@ export default function Header() {
    *
    */
 
+  // subcategory by mouse hover
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  
   // create nav links
   const subNavbarLinks = (
     <>
       {courseCategories?.map((category, index: number) => (
-        <li key={index} className="flex-shrink-0">
+        <li
+          onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          key={index}
+          className="relative flex-shrink-0 z-1 ">
           <Link
             href={`/courses/${category.category
               .trim()
               .toLowerCase()
               .replace(/\s+/g, "-")}`}>
-            <span className="text-black text-sm whitespace-nowrap px-3 py-0 block">
+            <span className=" text-black text-sm whitespace-nowrap px-3 py-0 block">
               {category.category}
             </span>
           </Link>
+          {/* showing dropdown link menu for subcategory */}
+          {hoveredIndex === index && (
+            <div className=" fixed mt-2 bg-white border rounded shadow-md z-10">
+              {category.subCategory.map((subCat, subIndex) => (
+                <div
+                  className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer whitespace-nowrap"
+                  key={subIndex}>
+                  {subCat}
+                  {/* <p>
+                  </p> */}
+                </div>
+              ))}
+            </div>
+          )}
         </li>
       ))}
     </>
   );
 
-  // courseCategories.forEach(([category, items]) => {
-  //   console.log("Category:", category); // string
-  //   items.forEach(item => console.log(" -", item)); // string[]
-  // });
-  //  courseCategories.map(([category, items], index) => {
-
-  //  })
-  // console.log(courseCategories.length);
+  courseCategories.map((category, index) => {
+    console.log(index, category, category.category, category.subCategory);
+    category.subCategory.forEach((sc, i) => {
+      console.log(i, sc);
+    });
+  });
+  console.log(courseCategories.length);
 
   return (
     //
