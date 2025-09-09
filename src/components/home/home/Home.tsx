9d1db3188542d8e1d4bbbd869693e1ef8259fd46
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-09 20:33:59 +0600

    updt

--- src/components/home/home/Home.tsx
+++ src/components/home/home/Home.tsx
@@ -20,24 +20,29 @@ export default function Home() {
      */
     <>
       {/* hero section in home page */}
-      <section className="w-full min-h-[55vh] bg-teal-100 border border-amber-200 p-12">
-          <div className=" w-[450px] flex flex-col bg-white p-6 border border-gray-300 drop-shadow-lg ">
-            <div className="flex flex-col gap-3">
-              <h1 className="text-black text-3xl font-semibold ">Master tomorrow&apos;s skills today</h1>
-              <p>
-                Power up your AI, career, and life skills with the most
-                up-to-date, expert-led learning.
-              </p>
-            </div>
-            <div className="flex gap-5 mt-4 ">
-              <button className="text-white font-semibold hover:bg-purple-500 border border-purple-500 bg-purple-700 rounded-sm px-5 py-3  " >Try it free</button>
-              <button className="text-purple-700 font-semibold hover:bg-purple-100 border border-purple-500  rounded-sm px-5 py-3  " >Learn AI</button>
-            </div>
+      <section className="flex flex-row gap-5 items-center justify-center w-full min-h-[55vh] bg-teal-100 border border-amber-200 p-12">
+        <div className=" w-[450px] flex flex-col bg-white p-6 border border-gray-300 drop-shadow-lg ">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-black text-3xl font-semibold ">
+              Master tomorrow&apos;s skills today
+            </h1>
+            <p>
+              Power up your AI, career, and life skills with the most
+              up-to-date, expert-led learning.
+            </p>
           </div>
-          <div></div>
-          <div></div>
-        <div className="w-full h-full   ">
+          <div className="flex gap-5 mt-4 ">
+            <button className="text-white font-semibold hover:bg-purple-500 border border-purple-500 bg-purple-700 rounded-sm px-5 py-3  ">
+              Try it free
+            </button>
+            <button className="text-purple-700 font-semibold hover:bg-purple-100 border border-purple-500  rounded-sm px-5 py-3  ">
+              Learn AI
+            </button>
+          </div>
         </div>
+        <div className="w-[500px] bg-red-500 h-[400px]"></div>
+        <div></div>
+        <div className="w-full h-full   "></div>
       </section>
     </>
   );
